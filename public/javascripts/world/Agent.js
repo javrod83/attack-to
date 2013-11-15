c41443eaa9b4010913fa564f5e70c4cd08815afe
javier <javrod83@gmail.com>
2013-11-15 17:59:10 -0300

    agent stuff

--- public/javascripts/world/Agent.js
+++ public/javascripts/world/Agent.js
@@ -1,19 +1,46 @@
-define([],
+define([
+	"./common/Model"
+],
 
-function() {
+function(Model) {
 
+		// pseudo constructor
+		function Agent(word,name) {
 
-		function init(options) {
+			Model.call(this) ;
+			this._world = world ;
+			this._name  = name ;
 
-
-
+			this._life			= 100 ;
+			this._position		= { "x": 0 , "y": 0} ;
+			this._dimentions	= {"width":30,"height":80} ;
+			this._aim_angle		= 90 ; 
+			this._direction		= "left";
+			return this;
 		}
 
-		// API Publica
-		return {
+        // The Friend class extends the base Model class.
+        Agent.prototype = Object.create( Model.prototype );
 
-			get
+        Agent.prototype.getName = function()
+        	{
+        		return (this._name);
+        	}
+        
+        Agent.prototype.getPos = function()
+        	{
+        		return (this._position)
+        	}
 
-		};
+        Agent.prototype.setPos = function(new_pos)
+        	{
+        		this._position = new_pos ;
+        	}
+
+        Agent.prototype.
+
+		// return the constructor function
+		return (Agent));
 
 	});
+
